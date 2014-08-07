c14776c6fe7f954d9885affafc65094e855c8c17
Nick Noble <nickynoble@me.com>
2014-08-07 16:13:05 -0400

    tweak code spacing

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
-<?php get_header(); // Load header.php ?>
-		
-<?php if (have_posts()) : // Check for posts ?>
-	<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+		<?php get_header(); // Load header.php ?>
+				
+		<?php if (have_posts()) : // Check for posts ?>
+			<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
 
-		<article>
-		
-			<h2><a href="<?php the_permalink() ?>"><?php the_title();?></a></h2>
+				<article>
+				
+					<h2><a href="<?php the_permalink() ?>"><?php the_title();?></a></h2>
 
-			<?php if (has_post_thumbnail( $post_id )) : // If the post has a thumbnail, show it: ?>
-				<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
-			<?php endif; ?>
+					<?php if (has_post_thumbnail( $post_id )) : // If the post has a thumbnail, show it: ?>
+						<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
+					<?php endif; ?>
 
-			<?php the_content(); // Get the post body ?>
+					<?php the_content(); // Get the post body ?>
 
-			<ul> <!-- Meta Content for post, add and remove as needed -->
-				<li><?php the_time('F jS, Y'); // Show the date posted ?></li>
-				<li><?php the_category(''); // Lists the categories?></li>
-				<li><?php comments_number( $zero, $one, $more, $deprecated ); // Let's us know if there's comments / how many exist ?></li>
-			</ul>
+					<ul> <!-- Meta Content for post, add and remove as needed -->
+						<li><?php the_time('F jS, Y'); // Show the date posted ?></li>
+						<li><?php the_category(''); // Lists the categories?></li>
+						<li><?php comments_number( $zero, $one, $more, $deprecated ); // Let's us know if there's comments / how many exist ?></li>
+					</ul>
 
-		</article>
+				</article>
 
-	<?php endwhile; // There are no more posts to list ?>
+			<?php endwhile; // There are no more posts to list ?>
 
-<?php else : // If no posts show up, tell the user ?>
-	<h2>No posts found :(</h2>
-<?php endif; ?>
+		<?php else : // If no posts show up, tell the user ?>
+			<h2>No posts found :(</h2>
+		<?php endif; ?>
 
-<?php get_footer(); // Load footer.php ?>
+		<?php get_footer(); // Load footer.php ?>
--- page.php
+++ page.php
@@ -1,17 +1,17 @@
-<?php get_header(); // Load header.php ?>
+		<?php get_header(); // Load header.php ?>
 
-<article>
-	
-	<?php if (have_posts()) : // Check for posts ?>
-		<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
-
-			<h2><?php the_title();?></h2>
+		<article>
 			
-			<?php the_content(); // Get the post body ?>
-	
-		<?php endwhile; ?>
-	<?php endif; ?>
+			<?php if (have_posts()) : // Check for posts ?>
+				<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
 
-</article>
+					<h2><?php the_title();?></h2>
+					
+					<?php the_content(); // Get the post body ?>
+			
+				<?php endwhile; ?>
+			<?php endif; ?>
 
-<?php get_footer(); // Load footer.php ?>
+		</article>
+
+		<?php get_footer(); // Load footer.php ?>
