2da109078608079b33159e6a1c140429b1a05bf4
Nick Noble <nickynoble@me.com>
2014-08-10 20:22:12 -0400

    add post_class and post_ID

--- single.php
+++ single.php
@@ -1,9 +1,9 @@
 <?php get_header(); // Load header.php ?>
-		
-<article>
 	
-	<?php if (have_posts()) : // Check for posts ?>
-		<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+<?php if (have_posts()) : // Check for posts ?>
+	<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 	
 			<h2><?php the_title();?></h2>
 
@@ -12,10 +12,10 @@
 			<?php endif; ?>
 
 			<?php the_content(); // Get the post body ?>
-	
-		<?php endwhile; ?>
-	<?php endif; ?>
+			
+		</article>
 
-</article>
+	<?php endwhile; ?>
+<?php endif; ?>
 
 <?php get_footer(); // Load footer.php ?>
